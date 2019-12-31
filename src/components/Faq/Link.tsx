d39c0cc08053f6ccf1970ca4184691301d731ac1
Arun Kumar Selvaraj <dev@ioak.org>
2019-12-31 14:59:38 +0530

    FAQ component styling adjustments

--- src/components/Faq/Link.tsx
+++ src/components/Faq/Link.tsx
@@ -22,24 +22,17 @@ class Link extends Component<Props, State> {
     }
 
     render() {
-        const tags: any = [];
-        if (this.props.faq.tags) {
-            this.props.faq.tags.split(" ").map(item => {
-                tags.push(<div className="tag" key={item} onClick={() => this.props.search(item)}>{item}</div>);
-            })
-        }
-        
         return (
             <div>
-                <div className="title typography-4">{this.props.faq.category}
+                <div className="title typography-4">
+                    <div className="tag" key={this.props.faq.category} onClick={() => this.props.search(this.props.faq.category)}>{this.props.faq.category}</div>
+                    {this.props.faq.question}
                     <div className="action-icon">
                         <i onClick={this.edit} className="material-icons">edit</i>
                         <i onClick={this.delete} className="material-icons">delete</i>
                     </div>
                 </div>
-                <div className="url typography-6"><a target="_blank" rel="noopener noreferrer" href={this.props.faq.href}>{this.props.faq.question}</a></div>
-                <div className="timestamp typography-6 space-bottom-1">{this.props.faq.answer}</div>
-                {tags}
+                <div className="typography-6">{this.props.faq.answer}</div>
             </div>
         )
     }
