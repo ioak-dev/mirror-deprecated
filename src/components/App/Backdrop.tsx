1cef588f357f56d1e8bf97b70b07813507cdb3c4
Arun Kumar Selvaraj <dev@ioak.org>
2020-02-24 17:41:22 +0530

    test config setup

--- src/components/App/Backdrop.tsx
+++ src/components/App/Backdrop.tsx
@@ -1,48 +1,22 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { receiveMessage } from '../../events/MessageService';
-import { Message } from '../Types/GeneralTypes';
 
-interface Props {
-}
-
-interface State {
-    backdrop: string
-}
-
-class Backdrop extends Component<Props, State> {
-    constructor(props: Props) {
-        super(props);
-        this.state = {
-            backdrop: ''
-        }
-    }
-
-    componentDidMount() {
-        receiveMessage().subscribe(
-            message => {
-                if (message.name === 'dialog') {
-
+const Backdrop = () => {
+    const [backdrop, setBackdrop] = useState('');
+    useEffect(() => {
+        const eventBus = receiveMessage().subscribe(message => {
+            if (message.name === 'dialog') {
+                if (message.signal) {
+                    setBackdrop('backdrop-fade');
+                } else {
+                    setBackdrop('');
                 }
             }
-        )
-        // if (nextProps.event && nextProps.event.name === 'dialog') {
-        //     if (nextProps.event.signal) {
-        //         this.setState({
-        //             backdrop: 'backdrop-fade'
-        //         })
-        //     } else {
-        //         this.setState({
-        //             backdrop: ''
-        //         })
-        //     }
-        // }
-    }
+        });
+        return () => eventBus.unsubscribe();
+    }, []);
 
-    render() {
-        return (
-            <div className={this.state.backdrop}></div>
-        );
-    }
-}
+    return <div className={backdrop} />;
+};
 
 export default Backdrop;
--- src/components/App/Content.tsx
+++ src/components/App/Content.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {useEffect} from 'react';
 import { Route } from 'react-router-dom';
 import './style.scss';
 import Home from '../Home';
@@ -7,7 +7,6 @@ import Login from '../Auth/Login';
 import Landing from '../Landing';
 import PrivateRoute from '../Auth/PrivateRoute';
 import AuthInit from '../Auth/AuthInit';
-import PropTypes from 'prop-types';
 import { withCookies } from 'react-cookie';
 import { connect } from 'react-redux';
 import { getAuth, addAuth, removeAuth } from '../../actions/AuthActions';
@@ -71,80 +70,74 @@ interface State {
     event: any
 }
 
-class Content extends Component<Props, State> {
-    constructor(props: Props) {
-        super(props);
-        this.props.getProfile();
-        this.props.getAuth();
-        this.props.getUser();
+const Content = (props: Props) => {
+    useEffect(() => {
+        props.getProfile();
+        props.getAuth();
+        props.getAuth();
+    }, []);
 
-        this.state = {
-            authorization: {
-                isAuth: false
-            },
-            event: {},
-            profile: {}
-        }
-    }
+    useEffect(() => {
+        const eventBus = receiveMessage().subscribe(message => {
+            if (message.name === 'session expired') {
+                logout(null, 'failure', 'Session expired. Login again');
+            }
+        });
+        return () => eventBus.unsubscribe();
+    });
 
-    componentWillReceiveProps(nextProps) {
-        if (nextProps.authorization && !this.props.authorization['token'] && nextProps.authorization.token && nextProps.profile.tenant) {
+    useEffect(() => {
+        if (props.authorization && props.authorization.token && props.profile.tenant) {
             httpGet(constants.API_URL_USER + '/' + 
-            nextProps.profile.tenant + '/',
+            props.profile.tenant + '/',
             {
                 headers:{
-                Authorization: nextProps.authorization.token
+                Authorization: props.authorization.token
                 }
             })
             .then((response) => {
                 console.log(response.data.data[0]);
-                this.props.addUser(response.data.data[0]);
+                props.addUser(response.data.data[0]);
             })
             
         }
-    }
-
-    componentDidMount() {
-        receiveMessage().subscribe();
-    }
+    }, [props.authorization]);
     
-    logout = (event: any, type = 'success', message = 'You have been logged out') => {
-        this.props.removeAuth();
-        this.props.cookies.remove('isAuth');
-        this.props.cookies.remove('token');
-        this.props.cookies.remove('secret');
-        this.props.cookies.remove('name');
+    const logout = (event: any, type = 'success', message = 'You have been logged out') => {
+        props.removeAuth();
+        props.cookies.remove('isAuth');
+        props.cookies.remove('token');
+        props.cookies.remove('secret');
+        props.cookies.remove('name');
         sendMessage('notification', true, {type: type, message: message, duration: 3000});
     }
 
-    render() {
-        return (
-            <div className={"App"}>
-                
-                <HashRouter>
-                    <AuthInit />
-                    <Backdrop />
-                    <div className="body">
-                        <div className="body-content">
-                            <Notification />
-                            <MuiThemeProvider theme={themes['themecolor_1']}>
-                                <Navigation {...this.props} logout={() => this.logout} event={this.state.event} />
-                                <Route path="/:tenant/home" render={(props: any) => <Home {...props} {...this.props} logout={() => this.logout} event={this.state.event} />} />
-                                <Route path="/:tenant/login" render={(props: any) => <Login {...props} {...this.props} logout={() => this.logout} event={this.state.event} />} />
-                                <Route path="/" exact render={(props: any) => <Landing {...props} {...this.props} logout={() => this.logout} event={this.state.event} />} />
-                                <Route path="/home" exact render={(props: any) => <Landing {...props} {...this.props} logout={() => this.logout} event={this.state.event} />} />
-                                <Route path="/tenant" exact render={(props: any) => <Tenant {...props} {...this.props} logout={() => this.logout} event={this.state.event} />} />
-                                <PrivateRoute path="/:tenant/settings" render={(props: any) => <Settings {...props} {...this.props} logout={() => this.logout} event={this.state.event} />} />
-                                <PrivateRoute path="/:tenant/faq" render={(props: any) => <Faq {...props} {...this.props} logout={() => this.logout} event={this.state.event} />} />
-                                <PrivateRoute path="/:tenant/servicerequests" render={(props: any) => <ServiceRequests {...props} {...this.props} logout={() => this.logout} event={this.state.event} />} />
-                                <PrivateRoute path="/:tenant/useradministration" render={(props: any) => <UserAdministration {...props} {...this.props} logout={() => this.logout} event={this.state.event} />} />
-                            </MuiThemeProvider>
-                        </div>
+    return (
+        <div className={"App"}>
+
+            <HashRouter>
+                <AuthInit />
+                <Backdrop />
+                <div className="body">
+                    <div className="body-content">
+                        <Notification />
+                        <MuiThemeProvider theme={themes['themecolor_1']}>
+                            <Navigation {...props} logout={() => logout} />
+                            <Route path="/:tenant/home" render={(propsLocal: any) => <Home {...propsLocal} {...props} logout={() => logout} />} />
+                            <Route path="/:tenant/login" render={(propsLocal: any) => <Login {...propsLocal} {...props} logout={() => logout} />} />
+                            <Route path="/" exact render={(propsLocal: any) => <Landing {...propsLocal} {...props} logout={() => logout} />} />
+                            <Route path="/home" exact render={(propsLocal: any) => <Landing {...propsLocal} {...props} logout={() => logout} />} />
+                            <Route path="/tenant" exact render={(propsLocal: any) => <Tenant {...propsLocal} {...props} logout={() => logout} />} />
+                            <PrivateRoute path="/:tenant/settings" render={(propsLocal: any) => <Settings {...propsLocal} {...props} logout={() => logout} />} />
+                            <PrivateRoute path="/:tenant/faq" render={(propsLocal: any) => <Faq {...propsLocal} {...props} logout={() => logout} />} />
+                            <PrivateRoute path="/:tenant/servicerequests" render={(propsLocal: any) => <ServiceRequests {...propsLocal} {...props} logout={() => logout} />} />
+                            <PrivateRoute path="/:tenant/useradministration" render={(propsLocal: any) => <UserAdministration {...propsLocal} {...props} logout={() => logout} />} />
+                        </MuiThemeProvider>
                     </div>
-                </HashRouter>
-            </div>
-        );
-    }
+                </div>
+            </HashRouter>
+        </div>
+    );
 }
 
 const mapStateToProps = (state: any) => ({
