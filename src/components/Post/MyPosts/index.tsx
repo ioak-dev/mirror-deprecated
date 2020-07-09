dfd1a3dd9ca1be47a52325a7c165ae5359385e9f
Joshi <shrinivas87.joshi@gmail.com>
2020-07-09 23:20:28 +0530

    cinderella-7: created myposts

--- src/components/Post/MyPosts/index.tsx
+++ src/components/Post/MyPosts/index.tsx
@@ -1,49 +1,22 @@
-import React, { useState, useEffect } from 'react';
-import { useSelector } from 'react-redux';
+import React from 'react';
 import { useQuery } from '@apollo/react-hooks';
 import OakPage from '../../../oakui/OakPage';
 import OakSection from '../../../oakui/OakSection';
 import OakSpinner from '../../../oakui/OakSpinner';
 import { MY_POSTS } from '../../Types/PostSchema';
-import StatusView from '../ViewPost/StatusView';
-import FollowerView from '../ViewPost/FollowerView';
 import OakHeading from '../../../oakui/OakHeading';
-import FeedbackView from '../ViewPost/FeedbackView';
-import MyPostsItem from './MyPostsItem';
-import TagContainer from '../ViewPost/TagContainer';
+import OakViewer from '../../../oakui/OakViewer';
 
-interface Props {
-  location: any;
-  history: any;
-  asset: string;
-}
-
-const queryString = require('query-string');
-
-const MyPosts = (props: Props) => {
+const MyPosts = () => {
   const { loading, error, data } = useQuery(MY_POSTS, {
     variables: { pageSize: 10, pageNo: 0 },
     fetchPolicy: 'cache-and-network',
   });
-  const goBack = () => {
-    props.history.goBack();
-  };
-
-  const getHeadingLinks = () => {
-    const links: any[] = [];
-    if (props.history.length > 2) {
-      links.push({
-        label: 'Go back',
-        icon: 'reply',
-        action: () => goBack(),
-      });
-    }
-    return links;
-  };
 
   return (
     <OakPage>
       <OakSection>
+        <OakHeading title="My posts" />
         {loading && <OakSpinner />}
         {!loading && !error && (
           <>
@@ -51,12 +24,7 @@ const MyPosts = (props: Props) => {
               <>
                 <OakHeading title={item.title || ''} linkSize="large" />
 
-                <MyPostsItem
-                  history={props.history}
-                  id={item.id}
-                  asset={props.asset}
-                  post={item}
-                />
+                <OakViewer>{item.description}</OakViewer>
               </>
             ))}
           </>
